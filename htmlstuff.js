9c441b5869bbca9e976023564f71fb3a446d9d3e
Rachel Joseph <racheljoseph@Rachels-MacBook-Pro.local>
2018-03-21 14:29:32 +0000

    challanges

---
+++ htmlstuff.js
@@ -0,0 +1,20 @@
+function square() {
+	window.alert(document.getElementById("number").value**2);
+}
+document.write(square(3));
+
+function sum (a, b, c) {
+return a + b + c;
+}
+document.write(sum(5,6,7));
+var person = {
+	name: "Jack",
+	age: "24",
+	occupation: "Student",
+};
+
+document.write(person.name + ", " + person.age + ", " + person.occupation);
+
+function person () {
+
+}
---
+++ q1.html
@@ -0,0 +1,7 @@
+<script src="htmlstuff.js" > </script>
+
+	<input id="number" type="number" placeholder="enter number to square here.." />
+	<input type="button" onclick="square()" value="square it" />
+
+</body>
+</html>
